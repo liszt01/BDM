9396bc9a39477765b99dba3fb3841e2e60d6ffa6
liszt01 <himuroland@gmail.com>
2024-01-17 14:31:47 +0900

    発表前の調整

--- soft/web/config.py
+++ soft/web/config.py
@@ -1,3 +1,4 @@
+# PI_IP = '172.20.10.5'
 PI_IP = '127.0.0.1'
 PI_PORT_SOCKET = 12345
 
@@ -5,12 +6,12 @@ PI_PORT_SOCKET = 12345
 INCAM = True
 # Flaskのデバッグモード
 # モーターの絶対角度指定など
-DEBUG_FLASK = True
+DEBUG_FLASK = False
 
 ANGLE_COEFF = 2 # カメラの移動速度に影響
 MIN_ANGLE_X = 30
 MAX_ANGLE_X = 210 # ~210
-MIN_ANGLE_Y = 30
+MIN_ANGLE_Y = 45
 MAX_ANGLE_Y = 150 # ~120
 
 NUM_ROCKETS = 8
